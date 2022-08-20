79e51005d52f78f9e8d17fe23780ddc39c1a50f7
Chufretalas <socorrobiptbopt@gmail.com>
2022-08-19 22:14:06 -0300

    page layout and style done

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,7 @@
 "use strict";
-console.log("helloooooo1111");
+const menorValor = 0;
+const maiorValor = 1000;
+const menorSpan = document.getElementById("menor-valor");
+const maiorSpan = document.getElementById("maior-valor");
+menorSpan.innerText = menorValor.toString();
+maiorSpan.innerText = maiorValor.toString();
--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,1 +1,8 @@
-console.log("helloooooo1111")
+const menorValor = 0
+const maiorValor = 1000
+
+const menorSpan = document.getElementById("menor-valor")
+const maiorSpan = document.getElementById("maior-valor")
+
+menorSpan!.innerText = menorValor.toString()
+maiorSpan!.innerText = maiorValor.toString()
